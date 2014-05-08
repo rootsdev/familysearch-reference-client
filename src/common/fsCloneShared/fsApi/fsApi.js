752946cc2d4941852844bf1b1971de10f2b67407
Lynn Monson <lmpub01@gmail.com>
2014-05-08 13:31:30 -0600

    First whack at allowing the FamilySearch.init function to be called from outside Angular -- needs to be done in a better way than this, eventually.

--- src/common/fsCloneShared/fsApi/fsApi.js
+++ src/common/fsCloneShared/fsApi/fsApi.js
@@ -2,9 +2,9 @@
   'use strict';
   angular.module('fsCloneShared')
     .provider('fsApi', function fsApiProvider(){
-        var app_key = 'WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK';
-        var environment = 'sandbox';
-        var auth_callback = 'http://localhost:9000/#!/auth';
+        var app_key = '';
+        var environment = '';
+        var auth_callback = '';
 
         this.setAppKey = function(appKey) {
           app_key = appKey;
@@ -19,17 +19,19 @@
         };
 
         this.$get = ['$window', '$http', '$q', '$timeout', function ($window,$http,$q,$timeout){
-          $window.FamilySearch.init({
-              app_key: app_key,
-              environment: environment,
-              auth_callback: auth_callback,
-              http_function: $http,
-              deferred_function: $q.defer,
-              timeout_function: $timeout,
-              save_access_token: true,
-              auto_expire: true,
-              auto_signin: true
-          });
+            if ( app_key && environment && auth_callback ) {
+                $window.FamilySearch.init({
+                    app_key: app_key,
+                    environment: environment,
+                    auth_callback: auth_callback,
+                    http_function: $http,
+                    deferred_function: $q.defer,
+                    timeout_function: $timeout,
+                    save_access_token: true,
+                    auto_expire: true,
+                    auto_signin: true
+                });
+            }
 
           $window.FamilySearch.Person.prototype._isMale = function() {
             return this.$getDisplayGender() === 'Male';
