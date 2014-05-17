f0b65680f50390273e67854ade0dca8d71475ed8
Lynn Monson <lmpub01@gmail.com>
2014-05-17 16:12:07 -0600

    Initial refactoring of tree is complete.  fsTree and fsTreeNode are current.  I expect further splitting as we go forward.  For now, the desired person is passed all the way down to the scope of the fsTreeNode.

--- src/common/fsCloneShared/fsTree/fsTree.js
+++ src/common/fsCloneShared/fsTree/fsTree.js
@@ -13,86 +13,10 @@
             scope.config = {};
             scope.model = {};
 
-            scope.focus = function() {
-                if (scope.sourcesCount === null) {
-                    fsApi.getPersonSourceRefs(scope.person.id).then(function(response){
-                        scope.sourcesCount = response ? response.getSourceRefs().length : 0;
-                    });
-                }
-                if (scope.discussionsCount === null) {
-                    fsApi.getPersonDiscussionRefs(scope.person.id).then(function(response){
-                        scope.discussionsCount = response ? response.getDiscussionRefs().length : 0;
-                    });
-                }
-            };
-
             scope.navigateTo = function() {
                 $state.go('person', { personId: scope.person.id });
             };
         }
       };
-    })
-
-    .directive('fsTreeCoupleCard',function() {
-      return {
-        restrict: 'E',
-        scope: {
-          person: '=',
-          spouse: '='
-        },
-        templateUrl: 'fsCloneShared/fsTree/fsTreeCoupleCard.tpl.html'
-
-      };
-    })
-
-
-    .directive('fsTree1',function() {
-      return {
-        scope: {
-          person: '=',
-          spouse: '=',
-          expandable: '@'
-        },
-        templateUrl: 'fsCloneShared/fsTree/fsTree1.tpl.html',
-        controller: function() {
-        }
-
-      };
-    })
-
-    .directive('fsTree2',function() {
-      return {
-        scope: {
-          person: '=',
-          spouse: '='
-        },
-        templateUrl: 'fsCloneShared/fsTree/fsTree2.tpl.html'
-
-      };
-    })
-
-    .directive('fsTree3',function() {
-      return {
-        scope: {
-          person: '=',
-          spouse: '='
-        },
-        templateUrl: 'fsCloneShared/fsTree/fsTree3.tpl.html'
-
-      };
-    })
-
-    .directive('fsTree4',function() {
-      return {
-        scope: {
-          person: '=',
-          spouse: '='
-        },
-        templateUrl: 'fsCloneShared/fsTree/fsTree4.tpl.html'
-
-      };
-    })
-
-
-  ;
+    });
 })();
